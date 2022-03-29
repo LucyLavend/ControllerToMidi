09d6f73d6379e59726322386bb876fb89bcb1e24
LucyLavend <jongebloedlucy@gmail.com>
2022-03-29 12:09:08 +0200

    Update UI & add basic midi channel code

--- Code/ControllerInstrument.cs
+++ Code/ControllerInstrument.cs
@@ -1,21 +1,131 @@
 using Godot;
 using System;
+using Melanchall.DryWetMidi.Multimedia;
+using Melanchall.DryWetMidi.Core;
+using Melanchall.DryWetMidi.Common;
+using Melanchall.DryWetMidi.Interaction;
 
 public class ControllerInstrument : Control
 {
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+    [Export]
+    int ID = 0;
+    [Export]
+    Color BGColor = Colors.LightCoral;
 
-    // Called when the node enters the scene tree for the first time.
+    int midiChannel;
+    string port = "controller port";
+    Label debugText;
+    int rootNote = 65;
+    int CCValue = 0;
+
     public override void _Ready()
     {
-        
+        // Panel BGPanel = GetNode("MarginContainer/Panel") as Panel;
+        // BGPanel.GetStylebox("panel").BGColor = BGColor;
+        GD.Print(Godot.Input.GetJoyName(0));
+        GD.Print(Godot.Input.GetJoyGuid(0));
+
+        GetNode<Label>("MarginContainer/Panel/MarginContainer/HSplitContainer/GridContainer/ControllerNumber").Text = "#" + (ID + 1);
+
+        debugText = GetTree().Root.GetNode("Main/VBoxContainer/DebugText") as Label;
     }
 
-//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-//  public override void _Process(float delta)
-//  {
-//      
-//  }
+    private void OnEventSent(object sender, MidiEventSentEventArgs e)
+    {
+        var midiDevice = (MidiDevice)sender;
+        GD.Print($"Event sent to '{midiDevice.Name}' at {DateTime.Now}: {e.Event}");
+        debugText.Text = $"Event sent to '{midiDevice.Name}': {e.Event}";
+    }
+
+    public override void _Process(float delta)
+    {
+        // ListenForInput("C1A", rootNote);
+        // ListenForInput("C1X", rootNote + 2);
+        // ListenForInput("C1Y", rootNote + 3);
+        // ListenForInput("C1B", rootNote + 5);
+        // ListenForInput("C1BR", rootNote + 7);
+        // ListenForInput("C1BL", rootNote + 8);
+
+        // ListenForAnalog("C1LeftStickUp", 80);
+
+        ListenForInput("C1A", rootNote);
+        ListenForInput("C1B", rootNote + 2);
+        ListenForInput("C1Y", rootNote + 3);
+        ListenForInput("C1X", rootNote + 5);
+        ListenForInput("C1BL", rootNote + 7);
+        ListenForInput("C1BL", rootNote + 8);
+
+        ListenForAnalog("Tilt", 80);
+
+        //GD.Print(Input.GetActionStrength("Whammy"));
+    }
+
+    private void ListenForInput(string buttonName, int note)
+    {
+        if (Input.IsActionJustPressed(buttonName))
+        {
+            playNote(note, 120);
+        }
+        if (Input.IsActionJustReleased(buttonName))
+        {
+            stopNote(note);
+        }
+    }
+
+    private void ListenForAnalog(string actionName, int CC, int minValue = 0, int maxValue = 127)
+    {
+        float oldCCValue = CCValue;
+        CCValue = (int)MapValue(Input.GetActionStrength(actionName), 0f, 1f, 0f, 127f);
+        // CCValue = (int)MapValue(Input.GetActionStrength(actionName), 0f, 1f, 64f, 96f);
+        if (CCValue != oldCCValue)
+        {
+            CCEvent(CC, CCValue);
+        }
+
+    }
+
+    private void playNote(int midiNote, int velocity)
+    {
+        using (var outputDevice = OutputDevice.GetByName(port))
+        {
+            outputDevice.EventSent += OnEventSent;
+            NoteOnEvent NOnEvent = new NoteOnEvent((SevenBitNumber)midiNote, (SevenBitNumber)velocity);
+            NOnEvent.Channel = (FourBitNumber)midiChannel;
+            outputDevice.SendEvent(NOnEvent);
+        }
+    }
+
+    private void stopNote(int midiNote, int velocity = 0)
+    {
+        using (var outputDevice = OutputDevice.GetByName(port))
+        {
+            outputDevice.EventSent += OnEventSent;
+            NoteOffEvent NOffEvent = new NoteOffEvent((SevenBitNumber)midiNote, (SevenBitNumber)velocity);
+            NOffEvent.Channel = (FourBitNumber)midiChannel;
+            outputDevice.SendEvent(NOffEvent);
+        }
+    }
+
+    private void CCEvent(int CC, int value = 0)
+    {
+        using (var outputDevice = OutputDevice.GetByName(port))
+        {
+            outputDevice.EventSent += OnEventSent;
+
+            outputDevice.SendEvent(new ControlChangeEvent((SevenBitNumber)CC, (SevenBitNumber)value));
+        }
+    }
+
+    public void SetMidiChannel(int channel)
+    {
+        midiChannel = channel;
+    }
+
+    private float MapValue(float x, float x1, float x2, float y1, float y2)
+    {
+        var m = (y2 - y1) / (x2 - x1);
+        var c = y1 - m * x1; // point of interest: c is also equal to y2 - m * x2, though float math might lead to slightly different results.
+
+        return m * x + c;
+    }
 }
--- Code/Testing.cs
+++ Code/Testing.cs
@@ -7,108 +7,110 @@ using Melanchall.DryWetMidi.Interaction;
 
 public class Testing : Control
 {
-    string port = "controller port";
-    Label debugText;
-    int rootNote = 65;
-    int CCValue = 0;
+    // string port = "controller port";
+    // Label debugText;
+    // int rootNote = 65;
+    // int CCValue = 0;
 
-    public override void _Ready()
-    {
-        GD.Print(Godot.Input.GetJoyName(0));
-        GD.Print(Godot.Input.GetJoyGuid(0));
+    // public override void _Ready()
+    // {
+    //     GD.Print(Godot.Input.GetJoyName(0));
+    //     GD.Print(Godot.Input.GetJoyGuid(0));
 
-        debugText = GetNode("VBoxContainer/DebugText") as Label;
-    }
+    //     debugText = GetNode("VBoxContainer/DebugText") as Label;
+    // }
 
-    private void OnEventSent(object sender, MidiEventSentEventArgs e)
-    {
-        var midiDevice = (MidiDevice)sender;
-        GD.Print($"Event sent to '{midiDevice.Name}' at {DateTime.Now}: {e.Event}");
-        debugText.Text = $"Event sent to '{midiDevice.Name}': {e.Event}";
-    }
+    // private void OnEventSent(object sender, MidiEventSentEventArgs e)
+    // {
+    //     var midiDevice = (MidiDevice)sender;
+    //     GD.Print($"Event sent to '{midiDevice.Name}' at {DateTime.Now}: {e.Event}");
+    //     debugText.Text = $"Event sent to '{midiDevice.Name}': {e.Event}";
+    // }
 
-    public override void _Process(float delta)
-    {
-        // ListenForInput("C1A", rootNote);
-        // ListenForInput("C1X", rootNote + 2);
-        // ListenForInput("C1Y", rootNote + 3);
-        // ListenForInput("C1B", rootNote + 5);
-        // ListenForInput("C1BR", rootNote + 7);
-        // ListenForInput("C1BL", rootNote + 8);
+    // public override void _Process(float delta)
+    // {
+    //     // ListenForInput("C1A", rootNote);
+    //     // ListenForInput("C1X", rootNote + 2);
+    //     // ListenForInput("C1Y", rootNote + 3);
+    //     // ListenForInput("C1B", rootNote + 5);
+    //     // ListenForInput("C1BR", rootNote + 7);
+    //     // ListenForInput("C1BL", rootNote + 8);
 
-        // ListenForAnalog("C1LeftStickUp", 80);
+    //     // ListenForAnalog("C1LeftStickUp", 80);
 
-        ListenForInput("C1A", rootNote);
-        ListenForInput("C1B", rootNote + 2);
-        ListenForInput("C1Y", rootNote + 3);
-        ListenForInput("C1X", rootNote + 5);
-        ListenForInput("C1BL", rootNote + 7);
-        ListenForInput("C1BL", rootNote + 8);
+    //     ListenForInput("C1A", rootNote);
+    //     ListenForInput("C1B", rootNote + 2);
+    //     ListenForInput("C1Y", rootNote + 3);
+    //     ListenForInput("C1X", rootNote + 5);
+    //     ListenForInput("C1BL", rootNote + 7);
+    //     ListenForInput("C1BL", rootNote + 8);
 
-        ListenForAnalog("Tilt", 80);
+    //     ListenForAnalog("Tilt", 80);
 
-        //GD.Print(Input.GetActionStrength("Whammy"));
-    }
+    //     //GD.Print(Input.GetActionStrength("Whammy"));
+    // }
 
-    public void ListenForInput(string buttonName, int note)
-    {
-        if (Input.IsActionJustPressed(buttonName))
-        {
-            playNote(note, 120);
-        }
-        if (Input.IsActionJustReleased(buttonName))
-        {
-            stopNote(note);
-        }
-    }
+    // public void ListenForInput(string buttonName, int note)
+    // {
+    //     if (Input.IsActionJustPressed(buttonName))
+    //     {
+    //         playNote(note, 120);
+    //     }
+    //     if (Input.IsActionJustReleased(buttonName))
+    //     {
+    //         stopNote(note);
+    //     }
+    // }
 
-    public void ListenForAnalog(string actionName, int CC, int minValue = 0, int maxValue = 127)
-    {
-        float oldCCValue = CCValue;
-        CCValue = (int)MapValue(Input.GetActionStrength(actionName), 0f, 1f, 0f, 127f);
-        // CCValue = (int)MapValue(Input.GetActionStrength(actionName), 0f, 1f, 64f, 96f);
-        if (CCValue != oldCCValue)
-        {
-            CCEvent(CC, CCValue);
-        }
+    // public void ListenForAnalog(string actionName, int CC, int minValue = 0, int maxValue = 127)
+    // {
+    //     float oldCCValue = CCValue;
+    //     CCValue = (int)MapValue(Input.GetActionStrength(actionName), 0f, 1f, 0f, 127f);
+    //     // CCValue = (int)MapValue(Input.GetActionStrength(actionName), 0f, 1f, 64f, 96f);
+    //     if (CCValue != oldCCValue)
+    //     {
+    //         CCEvent(CC, CCValue);
+    //     }
 
-    }
+    // }
 
-    public void playNote(int midiNote, int velocity)
-    {
-        using (var outputDevice = OutputDevice.GetByName(port))
-        {
-            outputDevice.EventSent += OnEventSent;
+    // public void playNote(int midiNote, int velocity)
+    // {
+    //     using (var outputDevice = OutputDevice.GetByName(port))
+    //     {
+    //         outputDevice.EventSent += OnEventSent;
+    //         NoteOnEvent NOnEvent = new NoteOnEvent((SevenBitNumber)midiNote, (SevenBitNumber)velocity);
+    //         NOnEvent.Channel = (FourBitNumber)1;
+    //         outputDevice.SendEvent(NOnEvent);
+    //     }
+    // }
 
-            outputDevice.SendEvent(new NoteOnEvent((SevenBitNumber)midiNote, (SevenBitNumber)velocity));
-        }
-    }
+    // public void stopNote(int midiNote, int velocity = 0)
+    // {
+    //     using (var outputDevice = OutputDevice.GetByName(port))
+    //     {
+    //         outputDevice.EventSent += OnEventSent;
+    //         NoteOffEvent NOffEvent = new NoteOffEvent((SevenBitNumber)midiNote, (SevenBitNumber)velocity);
+    //         NOffEvent.Channel = (FourBitNumber)1;
+    //         outputDevice.SendEvent(NOffEvent);
+    //     }
+    // }
 
-    public void stopNote(int midiNote, int velocity = 0)
-    {
-        using (var outputDevice = OutputDevice.GetByName(port))
-        {
-            outputDevice.EventSent += OnEventSent;
+    // public void CCEvent(int CC, int value = 0)
+    // {
+    //     using (var outputDevice = OutputDevice.GetByName(port))
+    //     {
+    //         outputDevice.EventSent += OnEventSent;
 
-            outputDevice.SendEvent(new NoteOffEvent((SevenBitNumber)midiNote, (SevenBitNumber)velocity));
-        }
-    }
+    //         outputDevice.SendEvent(new ControlChangeEvent((SevenBitNumber)CC, (SevenBitNumber)value));
+    //     }
+    // }
 
-    public void CCEvent(int CC, int value = 0)
-    {
-        using (var outputDevice = OutputDevice.GetByName(port))
-        {
-            outputDevice.EventSent += OnEventSent;
+    // public float MapValue(float x, float x1, float x2, float y1, float y2)
+    // {
+    //     var m = (y2 - y1) / (x2 - x1);
+    //     var c = y1 - m * x1; // point of interest: c is also equal to y2 - m * x2, though float math might lead to slightly different results.
 
-            outputDevice.SendEvent(new ControlChangeEvent((SevenBitNumber)CC, (SevenBitNumber)value));
-        }
-    }
-
-    public float MapValue(float x, float x1, float x2, float y1, float y2)
-    {
-        var m = (y2 - y1) / (x2 - x1);
-        var c = y1 - m * x1; // point of interest: c is also equal to y2 - m * x2, though float math might lead to slightly different results.
-
-        return m * x + c;
-    }
+    //     return m * x + c;
+    // }
 }
